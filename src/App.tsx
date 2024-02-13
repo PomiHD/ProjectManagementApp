7a2f8c5063c7c4727d000f8a248d6e3f42c6af6a
Samuel Yin <wsgddjy@live.com>
2024-02-13 23:01:31 +1000

    150. Styling Buttons & Inputs with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Sidebar from "./components/Sidebar.tsx";
 
+import {NewProject} from "./components/NewProject.tsx";
+
 function App() {
+  
   return (
     <>
-      <main className={"h-screen my-8"}>
-        <Sidebar />
+      <main className={"h-screen my-8 flex gap-8"}>
+        <Sidebar/>
+        <NewProject />
     </main>
     </>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function Sidebar() {
+  const [isClick, setIsClick] = useState(false);
+  function handleClick() {
+    setIsClick(!isClick);
+  }
   return (
     <>
       <aside>
@@ -7,6 +13,7 @@ function Sidebar() {
             + Your Projects
           </h2>
           <button
+            onClick={handleClick}
             className={
               "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:text-stone-100"
             }
