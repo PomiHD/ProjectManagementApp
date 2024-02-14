aeecfdac6c70034520c7ca82951258e3819d8d34
Samuel Yin <wsgddjy@live.com>
2024-02-14 19:31:24 +1000

    need to add advanced state management across components.

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,9 +1,15 @@
 import {Button} from "./Button.tsx";
+import {useState} from "react";
 
 export default function NewTask() {
+  const [enteredTask, setEnteredTask] = useState("");
+  function handleAddTask(event) {
+    setEnteredTask(event.target.value);
+    console.log(enteredTask);
+  }
   return <>
   <div className={"flex items-center gap-4 justify-end"}>
-    <input className={"w-full px-2 py-1 rounded-sm bg-stone-200"} type=" text" />
+    <input onChange={handleAddTask} value={enteredTask} className={"w-full px-2 py-1 rounded-sm bg-stone-200"} type=" text" />
     <Button>Add Task</Button>
   </div>
   </>;
