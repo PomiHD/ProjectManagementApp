695023f2a46f000a95012f46bc5e9dce9d02b261
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-14 03:34:03 +1000

    fix bug in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,20 @@
-import {ComponentPropsWithoutRef, ReactNode} from "react";
+import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 type ButtonProps = {
-    children: ReactNode;
-    } & ComponentPropsWithoutRef<"button">;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<"button">;
+
 export function Button({ children, ...props }: ButtonProps) {
   return (
-      <p>
-        <button
-          {...props}
-          className={
-            "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:text-stone-100"
-          }
-        >
-          {children}
-        </button>
-      </p>
-    
+    <p>
+      <button
+        {...props}
+        className={
+          "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:text-stone-100"
+        }
+      >
+        {children}
+      </button>
+    </p>
   );
 }
