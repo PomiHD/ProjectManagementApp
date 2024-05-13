81ab029c2f6f3ffc854ae293ed0cf97208fedd01
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-14 03:35:03 +1000

    fix bug

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,26 +1,27 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, ReactNode, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 import { Button } from "./Button.tsx";
 
-interface ModelProps {
-  children: any;
-  buttonCaption: string;
-}
-export interface ModelHandles {
+export type ModelHandles = {
   open: () => void;
-}
+};
+
+type ModelProps = {
+  children: ReactNode;
+  buttonCaption: string;
+};
 
 const Model = forwardRef<ModelHandles, ModelProps>(function Model(
   { children, buttonCaption },
   ref,
 ) {
-  const dialog = useRef<HTMLDialogElement | null>();
+  const dialog = useRef<HTMLDialogElement>(null);
   // useImperativeHandle is a hook that allows
   // you to customize the instance value that is exposed to parent components when using ref.
   useImperativeHandle(ref, () => {
     return {
       open() {
-        dialog.current?.showModal();
+        dialog.current!.showModal();
       },
     };
   });
@@ -37,7 +38,7 @@ const Model = forwardRef<ModelHandles, ModelProps>(function Model(
         <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
-    document.getElementById("modal-root"),
+    document.getElementById("modal-root")!,
   );
 });
 export default Model;
